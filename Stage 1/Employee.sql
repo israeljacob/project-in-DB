8d311e32da554735e4f5b197ff9a1a66cef3561c
Yisrael jacob <yisraeljakob@gmail.com>
2023-05-01 19:14:20 +0300

    fixes

---
+++ Stage 1/Employee.sql
@@ -0,0 +1,44 @@
+insert into IJACOB.EMPLOYEE (EMID, EMNAME, DATEOFBIRTH, ROLEID, TEID, CID, EmPhone, EmEmail)
+values (900, 'MikiBarrymore', to_date('05-07-2002', 'dd-mm-yyyy'), 150, 5, 315, 0536542816, 'miki@hfn.com');
+
+insert into IJACOB.EMPLOYEE (EMID, EMNAME, DATEOFBIRTH, ROLEID, TEID, CID, EmPhone, EmEmail)
+values (910, 'VictoriaDupree', to_date('17-09-1976', 'dd-mm-yyyy'), 225, 9, 314, 0523660895, 'victoria.dupree@uem.com');
+
+insert into IJACOB.EMPLOYEE (EMID, EMNAME, DATEOFBIRTH, ROLEID, TEID, CID, EmPhone, EmEmail)
+values (920, 'WillemBelles', to_date('25-02-1998', 'dd-mm-yyyy'), 150, 13, 304, 051893739, 'willem.belles@genghisgrill.au');
+
+insert into IJACOB.EMPLOYEE (EMID, EMNAME, DATEOFBIRTH, ROLEID, TEID, CID, EmPhone, EmEmail)
+values (930, 'RosannaFeore', to_date('28-11-2002', 'dd-mm-yyyy'), 200, 7, 311, 0531457495, 'rosanna.feore@royalgold.com');
+
+insert into IJACOB.EMPLOYEE (EMID, EMNAME, DATEOFBIRTH, ROLEID, TEID, CID, EmPhone, EmEmail)
+values (940, 'WesDuschel', to_date('14-01-1998', 'dd-mm-yyyy'), 425, 2, 311, 0514215033, 'wes.d@ezecastlesoftware.de');
+
+insert into IJACOB.EMPLOYEE (EMID, EMNAME, DATEOFBIRTH, ROLEID, TEID, CID, EmPhone, EmEmail)
+values (950, 'BobbiWeaving', to_date('31-03-1994', 'dd-mm-yyyy'), 375, 5, 307, 0552292873, 'bweaving@ntas.com');
+
+insert into IJACOB.EMPLOYEE (EMID, EMNAME, DATEOFBIRTH, ROLEID, TEID, CID, EmPhone, EmEmail)
+values (960, 'JulianaRyder', to_date('14-11-1981', 'dd-mm-yyyy'), 425, 2, 314, 0544381215, 'juliana.ryder@loreal.com');
+
+insert into IJACOB.EMPLOYEE (EMID, EMNAME, DATEOFBIRTH, ROLEID, TEID, CID, EmPhone, EmEmail)
+values (970, 'MurrayNoseworthy', to_date('09-12-1975', 'dd-mm-yyyy'), 275, 11, 314, 0504554326, 'mnoseworthy@sfgo.com');
+
+insert into IJACOB.EMPLOYEE (EMID, EMNAME, DATEOFBIRTH, ROLEID, TEID, CID, EmPhone, EmEmail)
+values (980, 'PabloShandling', to_date('28-07-1989', 'dd-mm-yyyy'), 175, 13, 306, 0551336703, 'pablos@unica.com');
+
+insert into IJACOB.EMPLOYEE (EMID, EMNAME, DATEOFBIRTH, ROLEID, TEID, CID, EmPhone, EmEmail)
+values (990, 'AlecKapanka', to_date('09-07-1978', 'dd-mm-yyyy'), 100, 3, 306, 0548118428, 'alec.kapanka@clorox.com');
+
+insert into IJACOB.EMPLOYEE (EMID, EMNAME, DATEOFBIRTH, ROLEID, TEID, CID, EmPhone, EmEmail)
+values (1000, 'DenzelBradford', to_date('23-12-1990', 'dd-mm-yyyy'), 325, 5, 309, 0501988384, 'denzel.bradford@atxforms.dk');
+
+insert into IJACOB.EMPLOYEE (EMID, EMNAME, DATEOFBIRTH, ROLEID, TEID, CID, EmPhone, EmEmail)
+values (1010, 'GeneNelligan', to_date('18-02-1973', 'dd-mm-yyyy'), 275, 13, 302, 0518200503, 'g.nelligan@staffforce.fr');
+
+insert into IJACOB.EMPLOYEE (EMID, EMNAME, DATEOFBIRTH, ROLEID, TEID, CID, EmPhone, EmEmail)
+values (1020, 'NedStone', to_date('17-04-1993', 'dd-mm-yyyy'), 225, 7, 302, 0514272506, 'n.stone@ris.de');
+
+insert into IJACOB.EMPLOYEE (EMID, EMNAME, DATEOFBIRTH, ROLEID, TEID, CID, EmPhone, EmEmail)
+values (1030, 'KaronTinsley', to_date('09-11-1985', 'dd-mm-yyyy'), 150, 11, 314, 051643292, 'ktinsley@wyeth.fr');
+
+commit;
+
